a0dbb09bc6f2adf6d15da42b68bc687e617155d5
kristof-kohegyi <121182064+kristof-kohegyi@users.noreply.github.com>
2023-09-22 12:04:55 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,22 +5,26 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa' // Hiányzó idézőjelek és pontosvessző
+    for (int i = 0;) // Nincs befejezve a kifejezés
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hiányzó érték 
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // Nincs megadva a kezdeti érték
+    for (int i = 0; i < N_ELEMENTS, i++) // Pontosvessző hiányzik
     {
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // Nincs memória felszabadítás
     return 0;
 }
+
+
+
